1369c47b15c7d9de682c46f447f94abf43a242ca
dvcvms <tetraedrtrapecia@gmail.com>
2023-02-14 06:47:49 +0000

    HM10 max

---
+++ src/main/java/ru/evsmanko/mankoff/controller/BankInfoController.java
@@ -0,0 +1,33 @@
+package ru.evsmanko.mankoff.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.evsmanko.mankoff.repository.UserRepository;
+
+@Controller
+@RequestMapping("/mankoff")
+public class BankInfoController {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public BankInfoController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/contacts")
+    public String getContactForm(Model model) {
+        model.addAttribute("people", userRepository.findAll());
+        return "contacts";
+    }
+
+    @GetMapping("/contacts/{id}")
+    public String showContactInformation(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", userRepository.getUserById(id));
+        return "show";
+    }
+}
