29cab79224460550f73b4dc176734e3a4be4ecd2
Arina Cherednik <arina.cherednik@yandex.ru>
2023-02-19 11:25:25 +0000

    Arina dz11

--- src/main/java/ru/evsmanko/mankoff/controller/AccountController.java
+++ src/main/java/ru/evsmanko/mankoff/controller/AccountController.java
@@ -1,13 +1,17 @@
 package ru.evsmanko.mankoff.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import ru.evsmanko.mankoff.repository.UserRepository;
 import ru.evsmanko.mankoff.service.BalanceService;
 import ru.evsmanko.mankoff.utils.FormattingUtils;
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
     @Autowired
     BalanceService balanceService;
@@ -15,11 +19,14 @@ public class AccountController {
     @Autowired
     FormattingUtils formattingUtils;
 
+    private final UserRepository userRepository;
     @GetMapping("/user")
-    public String userInfo() {
-        return "info";
+    public String userInfo(Model model) {
+        val users =  userRepository.findAll();
+        model.addAttribute("users", users);
+        return "userInf";
     }
-    
+
     @GetMapping("/balans")
     public String balances(Model model)
     {
