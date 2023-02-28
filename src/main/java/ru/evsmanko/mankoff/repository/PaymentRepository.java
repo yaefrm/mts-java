883d1dc1d05a3c2e3e75808eff7ac9307b0dc918
dvcvms <tetraedrtrapecia@gmail.com>
2023-02-28 07:19:25 +0000

    feat: inject SQL-queries into repository

--- src/main/java/ru/evsmanko/mankoff/repository/PaymentRepository.java
+++ src/main/java/ru/evsmanko/mankoff/repository/PaymentRepository.java
@@ -1,17 +1,45 @@
 package ru.evsmanko.mankoff.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.evsmanko.mankoff.entity.PaymentEntity;
 
 import java.util.List;
 
 @Repository
-public interface PaymentRepository extends CrudRepository<PaymentEntity, Long> {
+public class PaymentRepository {
 
-    List<PaymentEntity> findAll();
+    private final JdbcTemplate jdbcTemplate;
 
-    List<PaymentEntity> findPaymentEntitiesByBuyerId(long buyerId);
+    private static final String SQL_SELECT_ALL = "SELECT * FROM PAYMENTS";
 
-    PaymentEntity save(PaymentEntity payment);
+    private static final String SQL_SELECT_ALL_BY_BUYER_ID = "SELECT * FROM PAYMENTS WHERE BUYER_ID = ?";
+
+    private static final String SQL_INSERT = "INSERT INTO PAYMENTS (BUYER_ID, MCCCODE, SUM, TIMESTAMP) VALUES (?, ?, ?, ?)";
+
+    @Autowired
+    public PaymentRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<PaymentEntity> findAll() {
+        return jdbcTemplate.query(SQL_SELECT_ALL, new BeanPropertyRowMapper<>(PaymentEntity.class));
+    }
+
+    public List<PaymentEntity> findPaymentEntitiesByBuyerId(long buyerId) {
+        return jdbcTemplate.query(SQL_SELECT_ALL_BY_BUYER_ID, new BeanPropertyRowMapper<>(PaymentEntity.class), buyerId);
+    }
+
+    public PaymentEntity save(PaymentEntity payment) {
+        jdbcTemplate.update(
+                SQL_INSERT,
+                payment.getBuyerId(),
+                payment.getMCCCode(),
+                payment.getSum(),
+                payment.getTimestamp().toString());
+
+        return payment;
+    }
 }
