d23e8165d4e3b2fe0cae2dbf7d85414d01a11127
Efremova Yana <yana.com03@gmail.com>
2023-03-12 21:42:55 +0300

    add dto

---
+++ src/main/java/com/example/catalogfilm/converter/DirectorConverter.java
@@ -0,0 +1,25 @@
+package com.example.catalogfilm.converter;
+
+import com.example.catalogfilm.dto.DirectorDto;
+import com.example.catalogfilm.dto.FilmDto;
+import com.example.catalogfilm.model.Director;
+import com.example.catalogfilm.model.Film;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DirectorConverter {
+    private final ModelMapper modelMapper;
+
+    public DirectorConverter() {
+        modelMapper = new ModelMapper();
+    }
+
+    public DirectorDto convertToDto(Director mcc) {
+        return modelMapper.map(mcc, DirectorDto.class);
+    }
+
+    public Director convertToEntity(DirectorDto mcc){
+        return modelMapper.map(mcc, Director.class);
+    }
+}
---
+++ src/main/java/com/example/catalogfilm/converter/FilmConverter.java
@@ -0,0 +1,24 @@
+package com.example.catalogfilm.converter;
+
+import com.example.catalogfilm.dto.FilmDto;
+import com.example.catalogfilm.model.Film;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FilmConverter {
+
+    private final ModelMapper modelMapper;
+
+    public FilmConverter() {
+        modelMapper = new ModelMapper();
+    }
+
+    public FilmDto convertToDto(Film mcc) {
+        return modelMapper.map(mcc, FilmDto.class);
+    }
+
+    public Film convertToEntity(FilmDto mcc){
+        return modelMapper.map(mcc, Film.class);
+    }
+}
