373130bb299a2c15659950d9985b11b110169d6f
Efremova Yana <yana.com03@gmail.com>
2023-03-26 17:56:28 +0300

    add hw 19

--- src/main/java/com/example/catalogfilm/service/impl/DirectorServiceImpl.java
+++ src/main/java/com/example/catalogfilm/service/impl/DirectorServiceImpl.java
@@ -5,6 +5,8 @@ import com.example.catalogfilm.dto.DirectorDto;
 import com.example.catalogfilm.model.Director;
 import com.example.catalogfilm.repository.DirectorRepository;
 import com.example.catalogfilm.service.DirectorService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.hibernate.annotations.NotFound;
@@ -20,9 +22,19 @@ public class DirectorServiceImpl implements DirectorService {
     private final DirectorConverter directorConverter;
     @SneakyThrows({ChangeSetPersister.NotFoundException.class})
     @Override
+    @HystrixCommand(fallbackMethod = "getDefaultDirector",
+            commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000")
+    }
+    )
     public Director getDirector(UUID directorUuid) {
         Optional<Director> directorOptional= directorRepository.findById(directorUuid);
         return directorOptional.orElseThrow(() -> new ChangeSetPersister.NotFoundException());
+    }
+
+    public Director getDefaultDirector(UUID directorUuid) {
+        Director defaultDirector = new Director();
+        return defaultDirector;
     }
 
     @Override
